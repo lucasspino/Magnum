0fe2e1f64fce1e5b1430853641df0919af0d8dfa
lucasspino <lucaspino07@gmial.com>
2022-10-22 14:12:00 -0300

    layout finished

--- script.js
+++ script.js
@@ -17,3 +17,38 @@
         $menu.classList.remove("is-active");
     });
 })(document);
+
+/* *** CONTACT FORM *** */
+
+((d)=>{
+    const $form = d.querySelector(".contact-form"),
+        $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".contact-formresponse");
+
+        $form.addEventListener("submit", e=>{
+            e.preventDefault();
+            $loader.classList.remove("none");
+            fetch("https://formsubmit.co/ajax/magnumconstrucciones@outlook.com",{
+                method:"POST",
+                body: new FormData(e.target)    
+            })
+            .then(res=> (res.ok ? res.json() : Promise.reject(res)))
+            .then(json =>{
+                $loader.classList.add("none");
+                location.hash = "#gracias";
+                $form.reset();
+            })
+            .catch(err =>{
+                console.log(err)
+                let message = err.status.text || "Ocurrio un error al enviar, intenta nuevamente."
+                $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+            })
+            .finally(() => {
+                $loader.classList.add("none");
+                setTimeout(() => {
+                  location.hash = "#close";
+                }, 3000);
+            });
+    });
+
+})(document);
